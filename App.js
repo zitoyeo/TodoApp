6a229c693615bb8f99fc65c9ee2f964d8f886d8f
zitoyeo <wlsgh010203@gmail.com>
2022-03-09 22:34:21 +0900

    build TodoList & TodoItems

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
 import DateHead from "./components/DateHead";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import AddTodo from "./components/AddTodo";
 import Empty from "./components/Empty";
+import TodoList from "./components/TodoList";
 
 function App() {
   const today = new Date();
+
+  const [todos, setTodos] = useState([
+    { id: 1, text: "작업환경 설정", done: true },
+    { id: 2, text: "리액트 네이티브 기초 공부", done: false },
+    { id: 3, text: "투두리스트 만들기 ", done: false },
+  ]);
 
   return (
     <SafeAreaProvider>
@@ -16,6 +23,7 @@ function App() {
           style={styles.avoid}
         >
           <DateHead date={today} />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
           <Empty />
           <AddTodo />
         </KeyboardAvoidingView>
---
+++ components/TodoItem.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+function TodoItem({ id, text, done }) {
+  return (
+    <View style={styles.item}>
+      <View style={[styles.circle, done && styles.filled]} />
+      {done && (
+        <Image
+          source={require("../assets/icons/check_white/check_white.png")}
+        />
+      )}
+      <Text style={[styles.text, done && styles.lineThrough]}>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    padding: 16,
+    alignItems: "center",
+  },
+
+  circle: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    borderColor: "#26a69a",
+    borderWidth: 1,
+    marginRight: 16,
+  },
+
+  text: {
+    flex: 1,
+    fontSize: 16,
+    color: "#212121",
+  },
+  filled: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#26a69a",
+  },
+  lineThrough: {
+    color: "#9e9e9e",
+    textDecorationLine: "line-through",
+  },
+});
+
+export default TodoItem;
