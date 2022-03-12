76f5d14263ec5c27094ce1f553371542ee0385d7
zitoyeo <wlsgh010203@gmail.com>
2022-03-12 13:23:36 +0900

    update Alert & remove function

--- App.js
+++ App.js
@@ -37,6 +37,11 @@ function App() {
     setTodos(nextTodos);
   };
 
+  const onRemove = (id) => {
+    const nextTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={["bottom"]} style={styles.block}>
@@ -48,7 +53,7 @@ function App() {
           {todos.length === 0 ? (
             <Empty />
           ) : (
-            <TodoList todos={todos} onToggle={onToggle} />
+            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
           )}
           <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,7 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
-function TodoItem({ id, text, done, onToggle }) {
+function TodoItem({ id, text, done, onToggle, onRemove }) {
+  const remove = () => {
+    Alert.alert(
+      "삭제",
+      "정말로 삭제 하시겠습니까?",
+      [
+        { text: "취소", onPress: () => {}, style: "cancel" },
+        {
+          text: "삭제",
+          onPress: () => {
+            onRemove(id);
+          },
+          style: "destructive",
+        },
+      ],
+      {
+        cancleable: true,
+        onDissmiss: () => {},
+      }
+    );
+  };
+
   return (
     <View style={styles.item}>
       <TouchableOpacity onPress={() => onToggle(id)}>
@@ -14,6 +43,13 @@ function TodoItem({ id, text, done, onToggle }) {
         </View>
       </TouchableOpacity>
       <Text style={[styles.text, done && styles.lineThrough]}>{text}</Text>
+      {done ? (
+        <TouchableOpacity onPress={remove}>
+          <Icon name="delete" size={32} color="red" />
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.removePlaceholder} />
+      )}
     </View>
   );
 }
@@ -47,6 +83,10 @@ const styles = StyleSheet.create({
   lineThrough: {
     color: "#9e9e9e",
     textDecorationLine: "line-through",
+  },
+  removePlaceholder: {
+    width: 32,
+    height: 32,
   },
 });
 
